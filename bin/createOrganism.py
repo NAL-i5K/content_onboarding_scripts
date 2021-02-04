7b16d1c748de2ab7a53532293de7012239ad59e3
Vernon Chapman <vernon.chapman@usda.gov>
2021-02-03 23:06:35 -0500

    Refactored bin/createOrganism.py to make use of the python-apollo library for web services calls

--- bin/createOrganism.py
+++ bin/createOrganism.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python3
 from __future__ import absolute_import
-from subprocess import Popen, PIPE, call
-import json
 import sys
 import argparse
+import time
+from arrow.apollo import get_apollo_instance
 
 parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, description='''
 ''')
 parser.add_argument('-host', help="Host IP of Apollo2 server", required=True)
 parser.add_argument('-organism', help="Organism to be added", required=True)
-#parser.add_argument('-commonName', help="", required=True)
 parser.add_argument('-genus', help="Genus of added Organism", required=True)
 parser.add_argument('-species', help="Species of added Organism", required=True)
 parser.add_argument('-directory', help="Data directory of added Organism", required=True)
@@ -21,7 +20,6 @@ parser.add_argument('-password', help="", required=True)
  
 args = parser.parse_args()
 
-host = args.host
 organism = args.organism
 commonName = organism
 genus = args.genus
@@ -30,133 +28,94 @@ directory = args.directory
 blatdb = args.blatdb
 publicMode = args.publicMode
 token = args.token
-ADMIN = args.username
-PASSWD = args.password
-# Usage:
-# python createUserBatch.py add_users_groups.txt
 
+def organism_exists(commonName):
+    """Check if an organism exists
+    
+    Args:
+        commonName: Organism's Common Name
+    """
+    wa = get_apollo_instance()
+    exists = False
+    response = wa.organisms.get_organisms()
+    if isinstance(response, dict):
+        return exists
+    elif isinstance(response, list):
+        for item in response:
+            if item['commonName'] == commonName:
+                exists = True
+                break
+    else:
+        raise Exception("Unknown Response from Apollo")
 
-def create_findAllOrganism_str():
-    curl_str = ["curl", "-i", '-s', "-X", "POST", "-H", "'Content-Type:","application/json'", "-d"]
-    curl_cmd =  "'{\"username\":\"" + ADMIN + "\",\"password\":\"" + PASSWD + "\"}'"
-    curl_str.append(curl_cmd)
-    curl_str.append(host + "/apollo/organism/findAllOrganisms")
-    return ' '.join(curl_str)
+    return exists
 
-def create_insertOrganism_str(commonName, genus, species, directory, blatdb, publicMode, token):
-    curl_str = ["curl", "-i", '-s', "-X", "POST", "-H", "'Content-Type:","application/json'", "-d"]
-    curl_cmd =  "'{\"username\":\"" + ADMIN + "\",\"password\":\"" + PASSWD
-    curl_cmd += "\",\"commonName\":\"" + commonName + "\",\"genus\":\"" + genus + "\",\"species\":\"" + species
-    curl_cmd += "\",\"directory\":\"" + directory + "\",\"blatdb\":\"" + blatdb + "\",\"publicMode\":\"" + publicMode + "\",\"client_token\":\"" + token +"\"}'"
-    curl_str.append(curl_cmd)
-    curl_str.append(host + "/apollo/organism/addOrganism")
-    return ' '.join(curl_str)
+def create_organism(commonName, genus, species, directory, blatdb, publicMode, token):
+    """Create a new Organism 
 
-def create_loadGroups_str():
-    curl_str = ["curl", "-i", '-s', "-X", "POST", "-H", "'Content-Type:","application/json'", "-d"]
-    curl_cmd =  "'{\"username\":\"" + ADMIN + "\",\"password\":\"" + PASSWD + "\"}'"
-    curl_str.append(curl_cmd)
-    curl_str.append(host + "/apollo/group/loadGroups")
-    return ' '.join(curl_str)
+    Args:
+        commonName: Organism's Common Name
+        genus: Organism's Genus
+        species: Organism's Species
+        directory: Full Path to the Organism's JBrowse data
+        blatdb: Full path to blatdb file
+        publicMode: ( Boolean ) Should the Organism be visible publicly
+        token: Not really sure
 
-def create_createGroup_str(name, organism, token):
-    curl_str = ["curl", "-i", '-s', "-X", "POST", "-H", "'Content-Type:","application/json'", "-d"]
-    curl_cmd =  "'{\"username\":\"" + ADMIN + "\",\"password\":\"" + PASSWD
-    curl_cmd += "\",\"name\":\"" + name + "\",\"organism\":\"" + organism + "\",\"client_token\":\"" + token +"\"}'"
-    curl_str.append(curl_cmd)
-    curl_str.append(host + "/apollo/group/createGroup")
-    return ' '.join(curl_str)
+    """
+    wa = get_apollo_instance()
+    try:
+        orgs = wa.organisms.add_organism(
+            commonName,
+            directory,
+            blatdb=blatdb,
+            genus=genus,
+            species=species,
+            public=publicMode
+        )
+    except Exception as e:
+        print(f"Error Createing Organism '{organism}' see error below:")
+        print(e)
+        sys.exit(1)
 
-def create_updateOrganismPermission_str(name, organism, admin, read, write, export):
-    curl_str = ["curl", "-i", '-s', "-X", "POST", "-H", "'Content-Type:","application/json'", "-d"]
-    curl_cmd =  "'{\"username\":\"" + ADMIN + "\",\"password\":\"" + PASSWD
-    curl_cmd += "\",\"organism\":\"" + organism + "\",\"name\":\"" + name 
-    curl_cmd += "\",\"ADMINISTRATE\":\"" + admin + "\",\"READ\":\"" + read + "\",\"WRITE\":\"" + write + "\",\"EXPORT\":\"" + export +"\"}'"
-    curl_str.append(curl_cmd)
-    curl_str.append(host + "/apollo/group/updateOrganismPermission")
-    return ' '.join(curl_str)
+    return organism_exists(commonName)
 
+def create_groups(organism, genus, species):
+    """Create organism specific groups
 
-# ADD ORGANISM
-p = Popen(create_findAllOrganism_str(), stdout=PIPE, stderr=None, shell=True)
-output, err = p.communicate()
-msg = output.split('\n')[-1]
-d = json.loads(msg)
+    Args:
+        genus: Organism's Genus
+        species: Organism's Species
+    """
+    wa = get_apollo_instance()
+    existing_groups = [group.get("name") for group in wa.groups.get_groups()]
+    prefix = f"{genus}_{species}"
 
-is_organism_existed = False
-for data in d:
-    cName = data['commonName']
-    if cName == commonName:
-        is_organism_existed = True
-        o_id = data['id']
-        o_genus = data['genus']
-        o_species = data['species']
-        break
+    for postfix in ["ADMIN","USER","WRITE"]:
+        name = f"{prefix}_{postfix}"
+        if not name in existing_groups:
+            print(f"Creating Group '{name}'.")
+            group_response = wa.groups.create_group(f"{name}")
+            time.sleep(2)
+            if name in [wa.groups.get_groups(name=name)[0].get("name")]:
+                print(f"Group '{name}' successfully created.")
+                existing_groups.append(name)
 
-if is_organism_existed:
+        admin = read = write = export = False
+        if postfix == "USER":
+            read = True
+        elif postfix == "WRITE":
+            write = True
+
+        print(f"Update Group Permision for '{name}'")
+        perms = wa.groups.update_organism_permissions(name, organism, administrate=admin,
+                                              write=write, read=read, export=export)
+        time.sleep(1)
+    
+if organism_exists(commonName):
     print("Create organism: Organism %s existed" % (organism))
 else:
-    p = Popen(create_insertOrganism_str(commonName, genus, species, directory, blatdb, str(publicMode), token), stdout=PIPE, stderr=None, shell=True)
-    output, err = p.communicate()
-    msg = output.split('\n')[-1]
-    d = json.loads(msg)
-    if 'error' in d:
-        print("Create organism: fails, error:" + d['error'])
-        sys.exit()
-    else:
-        for data in d:
-            cName = data['commonName']
-            if cName == commonName:
-                o_id = data['id']
-                o_genus = data['genus']
-                o_species = data['species'] 
-                print("Create organism: organism %s created" % (organism))
+    print("Create organism: Organism %s does not exist" % (organism))
+    organism_created = create_organism(commonName, genus, species, directory, blatdb, publicMode, token)
 
-# ADD GROUPS
-p = Popen(create_loadGroups_str(), stdout=PIPE, stderr=None, shell=True)
-output, err = p.communicate()
-msg = output.split('\n')[-1]
-d = json.loads(msg)
-is_group_admin_existed = False
-is_group_read_existed = False
-is_group_write_existed = False
-group_admin_name = genus + '_' + species + "_ADMIN"
-group_read_name  = genus + '_' + species + "_USER"
-group_write_name = genus + '_' + species + "_WRITE"
-
-for data in d:
-    if data['name'] == group_admin_name:
-        print("Create group: group %s existed" % (group_admin_name))
-        is_group_admin_existed = True
-    elif data['name'] == group_read_name:
-        print("Create group: group %s existed" % (group_read_name))
-        is_group_read_existed = True
-    elif data['name'] == group_write_name:
-        print("Create group: group %s existed" % (group_write_name))
-        is_group_write_existed = True
-
-if not is_group_admin_existed:
-   p = Popen(create_createGroup_str(group_admin_name, organism, token), stdout=PIPE, stderr=None, shell=True) 
-   output, err = p.communicate()
-   print("Create group: group %s created" % (group_admin_name))
-
-if not is_group_read_existed:
-   p = Popen(create_createGroup_str(group_read_name, organism, token), stdout=PIPE, stderr=None, shell=True) 
-   output, err = p.communicate()
-   print("Create group: group %s created" % (group_read_name))
- 
-if not is_group_write_existed:
-   p = Popen(create_createGroup_str(group_write_name, organism, token), stdout=PIPE, stderr=None, shell=True) 
-   output, err = p.communicate()
-   print("Create group: group %s created" % (group_write_name))
- 
-# ADD PERMISSIONS
-print("Update permission")
-p = Popen(create_updateOrganismPermission_str(group_admin_name, organism, 'False', 'False', 'False', 'False'), stdout=PIPE, stderr=None, shell=True)
-output_p, err_p = p.communicate()
-
-p = Popen(create_updateOrganismPermission_str(group_read_name, organism, 'False', 'True', 'False', 'False'), stdout=PIPE, stderr=None, shell=True)
-output_p, err_p = p.communicate()
-
-p = Popen(create_updateOrganismPermission_str(group_write_name, organism, 'False', 'False', 'True', 'False'), stdout=PIPE, stderr=None, shell=True)
-output_p, err_p = p.communicate()
+create_groups(organism, genus, species)
